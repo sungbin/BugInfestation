fdaf6434044a93405983377cd4a9bd28169e05b4
sungbin <21700613@handong.edu>
2018-03-29 16:49:59 +0900

    declare local variables and functions

---
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -0,0 +1,45 @@
+package edu.handong.csee.java.lab06;
+
+public class BugInfestation {
+
+	static final double GROWTH_RATE = 0.95; //—weekly growth rate of the roach population
+	static final double ONE_BUG_VOLUME = 0.002; //—volume of an average roach (a constant 0.002)
+	double houseVolume; //— volume of the house
+	int startPopulation; //—initial number of roaches
+	int countWeeks; //—week counter
+	int population; //—current number of roaches
+	int totalBugVolume; //—total volume of all the roaches
+	int newBugs; //—number of roaches hatched this week
+	double newBugVolume; //—volume of new roaches
+	
+	void set(double h, int sP)
+	{
+		
+	}
+	
+	public boolean IsContinue()
+	{
+
+	}
+	
+	void NewBugs()
+	{
+
+	}
+	void NewBugVolume()
+	{
+
+	}
+	void Population()
+	{
+
+	}
+	void TotalBugVolume()
+	{
+
+	}
+	void CountWeeks()
+	{
+
+	}
+}
